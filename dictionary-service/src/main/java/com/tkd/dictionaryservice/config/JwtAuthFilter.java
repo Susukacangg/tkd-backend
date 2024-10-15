b289df60ca1542a6bff7032645f7db2d6636a948
Zachary <zjobby52@gmail.com>
2024-10-15 18:35:15 +0800

    Handle expired token differently

--- dictionary-service/src/main/java/com/tkd/dictionaryservice/config/JwtAuthFilter.java
+++ dictionary-service/src/main/java/com/tkd/dictionaryservice/config/JwtAuthFilter.java
@@ -1,11 +1,13 @@
 package com.tkd.dictionaryservice.config;
 
 import com.tkd.dictionaryservice.service.JwtService;
+import io.jsonwebtoken.ExpiredJwtException;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.lang.NonNull;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.context.SecurityContextHolder;
@@ -19,6 +21,7 @@ import java.io.IOException;
 
 @Component
 @RequiredArgsConstructor
+@Slf4j
 public class JwtAuthFilter extends OncePerRequestFilter {
 
     private final JwtService jwtService;
@@ -38,26 +41,30 @@ public class JwtAuthFilter extends OncePerRequestFilter {
             return;
         }
 
-        final String jwtToken, username;
-        jwtToken = authHeader.substring(7);
-        username = jwtService.extractUsername(jwtToken);
+        try {
+            final String jwtToken, username;
+            jwtToken = authHeader.substring(7);
+            username = jwtService.extractUsername(jwtToken);
 
-        // second statement checks if user is not yet authenticated
-        // tapi ada la tu user yang 'kunun' mau connect (first statement)
-        if(username != null && SecurityContextHolder.getContext().getAuthentication() == null) {
-            UserDetails userDetails = userDetailsService.loadUserByUsername(username);
+            // second statement checks if user is not yet authenticated
+            // tapi ada la tu user yang 'kunun' mau connect (first statement)
+            if (username != null && SecurityContextHolder.getContext().getAuthentication() == null) {
+                UserDetails userDetails = userDetailsService.loadUserByUsername(username);
 
-            // check if the token is valid or not
-            if(jwtService.isTokenValid(jwtToken, userDetails)) {
-                UsernamePasswordAuthenticationToken authToken = new UsernamePasswordAuthenticationToken(
-                        userDetails, null, userDetails.getAuthorities()
-                );
-                authToken.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
+                if(jwtService.isTokenValid(jwtToken, userDetails)) {
+                    UsernamePasswordAuthenticationToken authToken = new UsernamePasswordAuthenticationToken(
+                            userDetails, null, userDetails.getAuthorities()
+                    );
+                    authToken.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
 
-                SecurityContextHolder.getContext().setAuthentication(authToken);
+                    SecurityContextHolder.getContext().setAuthentication(authToken);
+                }
             }
+        } catch (ExpiredJwtException e) {
+            log.error(e.getMessage());
+        } finally {
+            filterChain.doFilter(request, response);
         }
 
-        filterChain.doFilter(request, response);
     }
 }
