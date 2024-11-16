d589068f1a78152ddc8981de514005700a9c8be7
Zachary <zjobby52@gmail.com>
2024-11-17 01:44:53 +0800

    Added allowed method

--- security-module/src/main/java/com/tkd/security/ModuleConfig.java
+++ security-module/src/main/java/com/tkd/security/ModuleConfig.java
@@ -28,7 +28,7 @@ public class ModuleConfig {
                         .allowedOrigins("https://localhost:5173", "https://thekadazandusundictionary.com")
                         .allowCredentials(true)
                         .allowedHeaders("*")
-                        .allowedMethods("GET", "POST", "PUT", "DELETE");
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "PATCH");
             }
         };
     }
